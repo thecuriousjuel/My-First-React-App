9bbbef606b4a0594823b9d9d92d98844b95f15a1
thecuriousjuel <juelbasak@gmail.com>
2023-07-03 21:25:36 +0530

    Can get data from form and save it

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,13 @@
 import './App.css';
 import React from "react";
+import { useState } from 'react';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App()
 {
-  const contacts = [
-    {
-      id: "1",
-      name: "Biswajit",
-      email: "bb@eg.com"
-    },
-    {
-      id: "2",
-      name: "Rohan",
-      email: "rr@eg.com"
-    },
-  ]
+  const [contacts, setContacts] = useState([]);
   return (
     <div className='ui container'>
       <Header />
